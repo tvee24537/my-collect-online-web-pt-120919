2c213b50b6057fc434a674a8bdbc47f26aeeb5fb
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-20 21:25:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(collection)
   i = 0
   collection = []
-  while i < array.length
-    collection << yield(array[i])
+  while i < collection.length
+    collected << yield(collection[i])
     i += 1
   end
-  collection
+  collected
 end
 
